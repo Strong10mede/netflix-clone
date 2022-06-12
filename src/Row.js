7c6a403cd772e74b1facaabbb6dafe50253953a8
Strong10mede <kmayur809@gmail.com>
2022-06-12 11:44:53 +0530

    commit

--- src/Row.js
+++ src/Row.js
@@ -6,7 +6,8 @@ function Row({ title, fetchUrl }) {
   //A snippet of code which runs based on a specific condition/variable
   useEffect(() => {
     //if [], run once when the row loads and dont run again
-  }, []);
+    //if [movies], var movies changes it will run again plus run once at start
+  }, [movies]);
 
   return (
     <div>
