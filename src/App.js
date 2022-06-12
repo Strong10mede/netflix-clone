735f90c134c203011885b1eba8ea8cd38a2cf628
Strong10mede <kmayur809@gmail.com>
2022-06-12 11:23:06 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Row from "./Row";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Building Netflix_Clone</h1>
+      <Row title="NETFLIX Originals" />
+      <Row title="Trending Now" />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Row.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Row({ title }) {
+  return <div>{title}</div>;
+}
+
+export default Row;
