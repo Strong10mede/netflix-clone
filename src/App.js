f27a47828f5250929899e4e6911dba1f598181c4
Strong10mede <kmayur809@gmail.com>
2022-06-12 11:31:39 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
 import Row from "./Row";
+import requests from "./requests";
 
 function App() {
   return (
     <div className="App">
       <h1>Building Netflix_Clone</h1>
-      <Row title="NETFLIX Originals" />
-      <Row title="Trending Now" />
+      <Row
+        title="NETFLIX Originals"
+        fetchUrl={requests.fetchNetflixOriginals}
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
     </div>
   );
 }
