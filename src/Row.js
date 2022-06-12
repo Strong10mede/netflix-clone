4acba1d614cab8be0f7cd4e9c8bd3ae894e0a3c4
Strong10mede <kmayur809@gmail.com>
2022-06-12 11:43:37 +0530

    commit

--- src/Row.js
+++ src/Row.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function Row({ title, fetchUrl }) {
-  return <div>{title}</div>;
+  const [movies, setMovies] = useState([]);
+
+  //A snippet of code which runs based on a specific condition/variable
+  useEffect(() => {
+    //if [], run once when the row loads and dont run again
+  }, []);
+
+  return (
+    <div>
+      <h2>{title}</h2>
+    </div>
+  );
 }
 
 export default Row;
