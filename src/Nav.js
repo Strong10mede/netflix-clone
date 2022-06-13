b5576a96269bfb5565b164d45946149ac465ebe3
Strong10mede <kmayur809@gmail.com>
2022-06-13 20:25:25 +0530

    commit

--- src/Nav.js
+++ src/Nav.js
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function Nav() {
+  const [show, handleShow] = useState([]);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else {
+        handleShow(false);
+      }
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
   return (
-    <div className="nav">
+    <div className={`nav ${show && "nav__black"}`}>
       <img className="nav__logo" src="" alt="Netflix Logo" />
       <img className="nav__avatar" src="" alt="Netflix Logo" />
     </div>
