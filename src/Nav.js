e2faae8e7b73114377f40307d11e10c07608c9d1
Strong10mede <kmayur809@gmail.com>
2022-06-13 20:03:12 +0530

    commit

--- src/Nav.js
+++ src/Nav.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 function Nav() {
-  return <div>Nav</div>;
+  return (
+    <div className="nav">
+      <img className="nav__logo" src="" alt="Netflix Logo" />
+      <img className="nav__avatar" src="" alt="Netflix Logo" />
+    </div>
+  );
 }
 
 export default Nav;
