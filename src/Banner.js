1309606f310569111bd833d43b1801ca7b20506d
Strong10mede <kmayur809@gmail.com>
2022-06-13 20:00:07 +0530

    commit

--- src/Banner.js
+++ src/Banner.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "./axios";
 import requests from "./requests";
+import "./Banner.css";
 function Banner() {
   const [movie, setMovie] = useState([]);
   useEffect(() => {
@@ -16,6 +17,9 @@ function Banner() {
     fetchData();
   }, []);
   console.log(movie);
+  function truncate(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  }
   return (
     <header
       className="banner"
@@ -26,18 +30,22 @@ function Banner() {
         backgroundPosition: "center center",
       }}
     >
-      <div className="banner__content">
-        {/*<<<background image*/}
+      <div className="banner__contents">
         {/* title */}
-        <h1>{movie?.title || movie?.name || movie?.orignal_name}</h1>
+        <h1 className="banner__title">
+          {movie?.title || movie?.name || movie?.orignal_name}
+        </h1>
         {/* div->2 buttons */}
         <div className="banner__buttons">
           <button className="banner__button">Play</button>
           <button className="banner__button">My List</button>
         </div>
         {/* description */}
-        <h1 className="banner__description">${movie?.overview}</h1>
+        <h1 className="banner__description">
+          {truncate(movie?.overview, 150)}
+        </h1>
       </div>
+      <div className="banner__fadeBottoms" />
     </header>
   );
 }
