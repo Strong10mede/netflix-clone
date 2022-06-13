c9304720f11104a55c6d2e9dfc37851f6761bfac
Strong10mede <kmayur809@gmail.com>
2022-06-13 09:41:15 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -11,6 +11,11 @@ function App() {
         fetchUrl={requests.fetchNetflixOriginals}
       />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      {/* <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Romantic Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} /> */}
     </div>
   );
 }
